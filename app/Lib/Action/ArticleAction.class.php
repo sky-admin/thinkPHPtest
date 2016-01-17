140bb008f47d2f461694322148e2932919c5932f
skyADMIN <546485299@qq.com>
2016-01-17 22:00:09 +0800

    文章编辑实现。还剩删除。

--- app/Lib/Action/ArticleAction.class.php
+++ app/Lib/Action/ArticleAction.class.php
@@ -6,6 +6,26 @@
  * Time: 下午11:56
  */
 class ArticleAction extends Action{
+    public function update(){
+        $Form   =   D('Article');
+        if($Form->create()) {
+            $result =   $Form->save();
+            if($result) {
+                $this->success('操作成功！');
+            }else{
+                $this->error('写入错误！');
+            }
+        }else{
+            $this->error($Form->getError());
+        }
+    }
+
+    public function edit($id=0){
+        $Form   =   D('Article');
+        $this->vo   =   $Form->find($id);
+        $this->display();
+    }
+
     public function read($id = 0){
         $Data = D('Article');
         $data = $Data->find($id);
--- app/Lib/Action/IndexAction.class.php
+++ app/Lib/Action/IndexAction.class.php
@@ -1,9 +1,10 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
-Load('extend');
+
 class IndexAction extends Action {
 
     public function index(){
+        Load('extend');
         $Data = M('Article');
         $list = $Data->order('publish_time DESC')->select();
         $this->assign('list',$list);
